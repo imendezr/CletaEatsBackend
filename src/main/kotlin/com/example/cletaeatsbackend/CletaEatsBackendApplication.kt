c3c161910f96c62ec32fc9930e656fbfe327198a
imendezr <isaacmendezr@gmail.com>
2025-06-19 08:51:27 -0600

    Base

---
+++ src/main/kotlin/com/example/cletaeatsbackend/CletaEatsBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cletaeatsbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CletaEatsBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<CletaEatsBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/cletaeatsbackend/CletaEatsBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cletaeatsbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CletaEatsBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
